0bdf2560dd920b651966a8c3cab209ff91edf131
abhinavaby <abhinavaby07@gmail.com>
2026-01-17 21:20:34 +0530

    Update typedef-keyword.c

--- typedef-keyword.c
+++ typedef-keyword.c
@@ -9,6 +9,9 @@ typedef struct sfspublickschoolandjuniorcollage{
 int main(){
     //struct sfspublicschoolandjuniourcollage s1
     sfs s1;
-    printf("%d %f %s",s1.roll,s1.cgpa);
+    s1.roll=1;
+    s1.cgpa=3.20;
+    strcpy(s1.name,"abhinav");
+    printf("%d %f %s",s1.roll,s1.cgpa,s1.name);
     return 0;
 }

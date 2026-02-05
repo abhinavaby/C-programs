58596d9839220ed1ee2c91558f1282272ef02926
abhinavaby <abhinavaby07@gmail.com>
2026-02-05 20:12:33 +0530

    Fix struct definition and initialize roll field

--- typedef-keyword.c
+++ typedef-keyword.c
@@ -2,15 +2,14 @@
 #include<string.h>
 typedef struct sfspublickschoolandjuniorcollage{
     int roll;
-    float c
+    float cgpa;
     char name[100];
 
 }sfs;
 int main(){
     //struct sfspublicschoolandjuniourcollage s1
     sfs s1;
-    s1.
-        
+    s1.roll=1;
     s1.cgpa=3.20;
     strcpy(s1.name,"abhinav");
     printf("%d %f %s",s1.roll,s1.cgpa,s1.name);

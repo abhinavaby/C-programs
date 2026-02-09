a630b8108ab710e9d8e097c0d50375d9041e9d56
abhinavaby <abhinavaby07@gmail.com>
2026-02-09 21:20:01 +0530

    Update string.c

---
+++ string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char firstname[20], lastname[20];
+    printf("enter your first name: ");
+    scanf("%s",firstname);
+    printf("enter your last name: ");   
+    scanf("%s",lastname);
+    printf("full name: "); 
+    for(int i=0;firstname[i]!='\0';i++){
+        printf("%c",firstname[i]);
+    }
+    printf(" ");
+    for(int i=0;lastname[i]!='\0';i++){
+        printf("%c",lastname[i]);
+    }
+    return 0;
+}

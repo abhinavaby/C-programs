ad0cc59c756ddee079c5942011f837c172f94b01
abhinavaby <abhinavaby07@gmail.com>
2026-03-01 22:03:55 +0530

    Fix include statements and formatting in string.c

--- string.c
+++ string.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h
 int main(){
     char firstname[20], lastname[20];
     printf("enter your first name: ");
@@ -16,3 +16,4 @@ int main(){
     }
     return 0;
 }
+

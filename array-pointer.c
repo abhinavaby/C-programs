0d141a7fe0598a5334d261b8c6fadd2c28a8e84b
abhinavaby <abhinavaby07@gmail.com>
2026-03-04 22:50:06 +0530

    Add files via upload

--- array-pointer.c
+++ array-pointer.c
@@ -1,25 +1,13 @@
-#include <stdio.h>
-
-// Function that accepts a pointer to the first element
-int sumArray(int *ptr, int size) {
-    int total = 0;
-
-    for (int i = 0; i < size; i++) {
-        total += *ptr; // Add the value pointed to
-        ptr++;         // Move pointer to the next memory address
+#include<stdio.h>
+int main(){
+    int a[]={1,2,3,4,5};
+    int *p=a;
+    printf("%p\n", p);
+    printf("%p\n", (p+1));
+    printf("%d\n", *p);
+    printf("%d\n", *(p+1));
+    for(int i=0;i<5;i++){
+        printf("%d ", *(p+i));
     }
-
-    return total;
-}
-
-int main() {
-    int myNumbers[] = {5, 10, 15, 20};
-    int length = sizeof(myNumbers) / sizeof(myNumbers[0]);
-
-    // Pass the array name (which acts as a pointer)
-    int result = sumArray(myNumbers, length);
-
-    printf("The total sum is: %d\n", result);
-
     return 0;
 }

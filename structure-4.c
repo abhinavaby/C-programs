b76b1e0544f89c6f10006209c30260a99e02d0cc
abhinavaby <abhinavaby07@gmail.com>
2026-03-07 21:29:26 +0530

    Add files via upload

---
+++ structure-4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+struct data{
+    char name[20];
+    int class;
+    char division;
+    float mark;
+
+};
+int main(){
+    int n;
+    printf("Enter the number of entries:");
+    scanf("%d",&n);
+    struct data s[n];
+    for(int i=0;i<n;i++){
+        printf("Enter the name:");
+        scanf("%s",s[i].name);
+        printf("Enter the class:");
+        scanf("%d",&s[i].class);
+        printf("Enter the division:");
+        scanf(" %c",&s[i].division);
+        printf("Enter the mark:");
+        scanf("%f",&s[i].mark);
+    }
+    printf("Name\tClass\tDivision\tMark\n");
+    for(int i=0;i<n;i++){
+        printf("%s\t%d\t%c\t\t%.2f\n",s[i].name,s[i].class,s[i].division,s[i].mark);
+    }
+    printf("students failed:\n");
+    printf("Name\tClass\tDivision\tMark\n");
+    for(int i=0;i<n;i++){
+        if(s[i].mark<40){
+            printf("%s\t%d\t%c\t\t%.2f\n",s[i].name,s[i].class,s[i].division,s[i].mark);
+        }
+    }
+    return 0;
+}
